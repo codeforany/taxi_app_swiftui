58bf1f719a43f7ec6a031117a7f7a72644271f66
codeforany <130132429+codeforany@users.noreply.github.com>
2023-09-17 12:04:41 +0530

    Initial Commit

---
+++ taxi_driver/taxi_driverApp.swift
@@ -0,0 +1,17 @@
+//
+//  taxi_driverApp.swift
+//  taxi_driver
+//
+//  Created by CodeForAny on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct taxi_driverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
