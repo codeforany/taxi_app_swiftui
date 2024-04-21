6dac9fcd617a06011e8cdc1c4212935968876996
CodeForAny <130132429+codeforany@users.noreply.github.com>
2024-04-21 10:41:41 +0530

    Tip Detail with Api Calling & Data Set

---
+++ taxi_driver/ViewModel/TipDetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TipDetailViewModel.swift
+//  taxi_driver
+//
+//  Created by CodeForAny on 20/04/24.
+//
+
+import SwiftUI
+
+struct TipDetailViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipDetailViewModel()
+}
