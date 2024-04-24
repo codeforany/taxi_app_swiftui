453f6995a2ae806454661179eb2c2baf2b070de3
CodeForAny <130132429+codeforany@users.noreply.github.com>
2024-04-25 02:10:19 +0530

    Driver Summary With Api Calling & Data Set

---
+++ taxi_driver/ViewModel/SummaryViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryViewModel.swift
+//  taxi_driver
+//
+//  Created by CodeForAny on 25/04/24.
+//
+
+import SwiftUI
+
+struct SummaryViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryViewModel()
+}
