9ea607c680bd4342ee01e6bb47b3addae0cf1837
CodeForAny <130132429+codeforany@users.noreply.github.com>
2024-04-29 23:13:49 +0530

    personal document upload with api calling

---
+++ taxi_driver/Model/DocumentModel.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentModel.swift
+//  taxi_driver
+//
+//  Created by CodeForAny on 27/04/24.
+//
+
+import SwiftUI
+
+struct DocumentModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentModel()
+}
